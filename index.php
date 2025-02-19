c12f646eb641b15aad35217598a37207bd146ed9
Okoruwu <jonathandeleonluna@gmail.com>
2025-02-19 16:40:21 -0600

    0

--- index.php
+++ index.php
@@ -8,15 +8,15 @@
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js"></script>
 
-  <title>Prueba</title>
-  <link rel="stylesheet" href="../css/stylenav.css">
-  <link rel="stylesheet" href="../css/styleind.css">
+  <title>700VALIENTES</title>
+  <link rel="stylesheet" href="css/styleind.css">
+  <link rel="stylesheet" href="css/styleind.css">
 </head>
 
 <body>
 
   <?php
-  include_once 'nav.php';
+  include_once 'modulos/nav.php';
   ?>
 
   <div class="hero-image">
@@ -26,43 +26,43 @@
     </div>
   </div>
 
-<div class="cardback">
+  <div class="cardback">
 
-<h3>¿Quiénes somos?</h3>
+    <h3>¿Quiénes somos?</h3>
 
-  <div class="container">
-    <div class="box">
-      <span></span>
-      <div class="content">
-        <h2 style="text-align: center;">Misión</h2>
-        <p style="text-align: justify;">Inspirar y tranformar vidas a través de valores y principios.</p>
+    <div class="container">
+      <div class="box">
+        <span></span>
+        <div class="content">
+          <h2 style="text-align: center;">Misión</h2>
+          <p style="text-align: justify;">Inspirar y tranformar vidas a través de valores y principios.</p>
+        </div>
       </div>
-    </div>
-    <div class="box">
-      <span></span>
-      <div class="content">
-        <h2 style="text-align: center;">Visión</h2>
-        <p style="text-align: justify;">Establecer una Feria Aeroespacial, de Seguridad y Defensa sustentable, con
-          prestigio y liderazgo en la comunidad aeronáutica mundial,
-          que promueva.</p>
+      <div class="box">
+        <span></span>
+        <div class="content">
+          <h2 style="text-align: center;">Visión</h2>
+          <p style="text-align: justify;">Establecer una Feria Aeroespacial, de Seguridad y Defensa sustentable, con
+            prestigio y liderazgo en la comunidad aeronáutica mundial,
+            que promueva.</p>
+        </div>
       </div>
-    </div>
 
-    <div class="box">
-      <span></span>
-      <div class="content">
-        <h2 style="text-align: center;">Objetivos</h2>
-        <p style="text-align: justify;">Establecer una Feria Aeroespacial, de Seguridad y Defensa sustentable, con
-          prestigio y liderazgo en la comunidad aeronáutica mundial,
-          que promueva.</p>
+      <div class="box">
+        <span></span>
+        <div class="content">
+          <h2 style="text-align: center;">Objetivos</h2>
+          <p style="text-align: justify;">Establecer una Feria Aeroespacial, de Seguridad y Defensa sustentable, con
+            prestigio y liderazgo en la comunidad aeronáutica mundial,
+            que promueva.</p>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
   <?php
 
-  include 'footer.php';
+  include 'modulos/footer.php';
   ?>
 
 </body>

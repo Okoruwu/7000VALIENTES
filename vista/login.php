df1e27d10119b1850aeb7f515d31cd9a4c098e5c
TWZedryd <monserratriverariv21@gmail.com>
2025-02-20 15:34:29 -0600

    Login

---
+++ vista/login.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./css/stlog.css">
+
+    <title>Iniciar Sesión</title>
+</head>
+<body>
+
+<p class="tip"></p>
+<div class="cont">
+  <div class="form sign-in">
+    <h2>¡Hola! Bienvenido De Nuevo</h2>
+ 
+    <label>
+      <span>Correo</span>
+      <input type="email" />
+    </label>
+    <label>
+      <span>Contraseña</span>
+      <input type="password" />
+    </label>
+    <button type="button" class="submit">Ingresar</button>
+
+  </div>
+  <div class="sub-cont">
+    <div class="img">
+
+      <div class="img__text m--in">
+        <h2>¿No puedes ingresar?</h2>
+        <p>Contacta a soporté para dar notificación sobre tu caso</p>
+      </div>
+      <div class="img__btn">
+        
+        <span class="m--up">¿Olvidaste algo?</span>
+        <span class="m--in">Iniciar Sesión</span>
+      </div>
+    </div>
+    <div class="form sign-up">
+      <h2>Ingresa lo siguiente</h2>
+      <label>
+        <span>Correo</span>
+        <input type="email" />
+      </label>
+      <label>
+        <span>Nombre</span>
+        <input type="text" />
+      </label>
+      <label>
+        <span>Motivo</span>
+        <input type="text" />
+      </label>
+      <button type="button" class="submit">Enviar</button>
+
+    </div>
+  </div>
+</div>
+    
+</body>
+
+<script src="js/log.js"></script>
+
+</html>

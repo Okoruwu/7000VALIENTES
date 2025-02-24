998cf2a829c355f8bd4d346691592af1acb79d3d
TWZedryd <monserratriverariv21@gmail.com>
2025-02-24 17:12:24 -0600

    title individuales

--- modulos/marquee.php
+++ modulos/marquee.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Marquee</title>
     <link rel="stylesheet" href="./css/marquee.css">
 </head>
 
--- modulos/nav.php
+++ modulos/nav.php
@@ -11,7 +11,6 @@ if (session_status() == PHP_SESSION_NONE) {
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Navbar</title>
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <link rel="stylesheet" href="./css/stylenav.css">

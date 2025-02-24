a4b811c6d40c55d0649bf28b722b0fe7e8ec0965
TWZedryd <monserratriverariv21@gmail.com>
2025-02-24 17:32:51 -0600

    style

--- calendario.php
+++ calendario.php
@@ -12,10 +12,7 @@ include_once './modulos/nav.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="./css/styleind.css">
-    <link rel="stylesheet" href="./css/stylefon.css">
     <link rel="stylesheet" href="./css/stylenav.css">
-    <link rel="stylesheet" href="./css/styleform.css">
     <link rel="stylesheet" href="./css/load.css">
     <link rel="stylesheet" href="./css/calendario.css">
 

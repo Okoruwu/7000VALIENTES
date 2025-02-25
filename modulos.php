8adf6a7f7bed45447110b7cf4959acbee0b707fe
TWZedryd <monserratriverariv21@gmail.com>
2025-02-25 12:41:06 -0600

    style modulos responsive

--- modulos.php
+++ modulos.php
@@ -8,10 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="./css/styleind.css">
-    <link rel="stylesheet" href="./css/stylefon.css">
     <link rel="stylesheet" href="./css/stylenav.css">
-    <link rel="stylesheet" href="./css/styleform.css">
     <link rel="stylesheet" href="./css/load.css">
     <link rel="stylesheet" href="./css/modulos.css">
 </head>
